af5367f0cf83550501f132f8906de2437d2800bc
James B. Crocker <james@constantsc.com>
2015-09-04 15:05:16 -0400

    Addressing gitbub comments

--- src/lib/perl/OVF/Automation/Vars.pm
+++ src/lib/perl/OVF/Automation/Vars.pm
@@ -42,11 +42,13 @@ our $vmDeviceIsoName         = 'iso';
 our $vmDeviceConnectableName = 'connectable';
 our $exportName              = 'export';
 our $discoverName            = 'discover';
-our $powerRegex              = qq{$powerOnName|$powerOffName|$suspendName|$resetName|$rebootName|$shutdownName};
+our $powerRegex              = qq{$powerOnName|$powerOffName|$suspendName};
+$powerRegex                  .= qq{|$resetName|$rebootName|$shutdownName};
 our $deviceRegex             = qq{$deviceAttachName|$deviceDetachName|$deviceListName};
 our $snapshotRegex           = qq{$snapshotCreateName|$snapshotRevertName|$snapshotDestroyName};
 our $vmDeviceRegex           = qq{$vmDeviceIsoName|$vmDeviceConnectableName};
-our $actionRegex             = qq{$deployName|$destroyName|$exportName|$discoverName|$powerRegex|$deviceRegex|$snapshotRegex};
+our $actionRegex             = qq{$deployName|$destroyName|$exportName};
+$actionRegex                 .= qq{|$discoverName|$powerRegex|$deviceRegex|$snapshotRegex};
 
 our $defaultOvftoolPath         = '/usr/bin/ovftool';
 our $defaultSnapshotDescription = 'FVORGE AUTOMATION';
@@ -56,9 +58,9 @@ our $ovftoolNoSslVerify         = '--noSSLVerify';
 our $propOverrideSplitter       = q{\s*;;;\s*};
 
 $common{'RHEL'} = {
-	'bin' => {
-		'ovftool'          => { 'path' => $defaultOvftoolPath },
-	}
+    'bin' => {
+        'ovftool'          => { 'path' => $defaultOvftoolPath },
+    }
 };
 
 $automate{'RHEL'}{5}{9}{'x86_64'} = Storable::dclone( $common{'RHEL'} );
